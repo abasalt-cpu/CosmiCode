617d819025e4b068975f2da23140c34970c34e97
abasalt-cpu <abasalt@gmail.com>
2026-07-10 20:07:23 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
-from app.core import logger
-
-logger.info("Starting CosmiCodeBot...")
 import asyncio
-from app.database import Base, engine
 from aiogram import Dispatcher
 
 from app.bot import router
 from app.core.bot import bot
+from app.core import logger
+from app.database import Base, engine
 
-from app.database.database import initialize_database
-
-initialize_database()
-
-async def main() -> None:
-    dp = Dispatcher()
-
-    dp.include_router(router)
-
-    print("🚀 CosmiCodeBot Started...")
 Base.metadata.create_all(bind=engine)
+
+async def main():
+    dp=Dispatcher()
+    dp.include_router(router)
+    logger.info("Bot is running...")
     await dp.start_polling(bot)
-logger.info("Bot is running...")
-if __name__ == "__main__":
+
+if __name__=="__main__":
+    logger.info("Starting CosmiCodeBot...")
     asyncio.run(main())
